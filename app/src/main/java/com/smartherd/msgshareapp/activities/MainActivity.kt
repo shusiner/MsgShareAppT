a1a1fef426226fbcee9a4738740fa808f6c2ae2f
shusiner <quantum.plato@gmail.com>
2018-10-24 10:19:20 +0800

    d

--- app/src/main/java/com/smartherd/msgshareapp/activities/MainActivity.kt
+++ app/src/main/java/com/smartherd/msgshareapp/activities/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
         btnShowToast.setOnClickListener {
             Log.i(TAG, "Button was clicked !")
             showToast("Button was clicked!")
-            showToast("Button was clicked!")
 
             //Toast.makeText(this,"Button was clicked!",Toast.LENGTH_SHORT).show()
         }
